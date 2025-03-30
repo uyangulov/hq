23bcf03f288faf7adb2fe3bda70b1dfca3c12f00
kirill.uyangulov <kirill.uyangulov@gmail.com>
2025-03-30 18:09:27 +0300

    update op_to_superop function

--- utils.py
+++ utils.py
@@ -1,25 +1,32 @@
 import numpy as np
 
 
+# def op_to_superop(op):
+#     if op.shape == (2, 2):  # 1-qubit case
+#         return np.einsum('ab,cd->abcd', op, op.conj())
+#     elif op.shape == (2, 2, 2, 2):  # 2-qubit case
+#         return np.einsum('abcd,efgh->abcdefgh', op, op.conj())
+#     else:
+#         raise ValueError("Invalid operator shape for superoperator conversion")
+
 def op_to_superop(op):
-    if op.shape == (2, 2):  # 1-qubit case
-        return np.einsum('ab,cd->abcd', op, op.conj())
-    elif op.shape == (2, 2, 2, 2):  # 2-qubit case
-        return np.einsum('abcd,efgh->abcdefgh', op, op.conj())
-    else:
-        raise ValueError("Invalid operator shape for superoperator conversion")
+    superop = np.kron(op, op.conj())
+    if op.shape == (2, 2):
+       return superop.reshape([2] * 4)
+    return superop.reshape([2] * 8)
+    
 
 
 def apply_superop_1q(rho, super_op, i):
     n = rho.ndim // 2
-    rho = np.tensordot(super_op, rho, axes=[[1, 3], [i, n+i]])
+    rho = np.tensordot(super_op, rho, axes=[[2, 3], [i, n+i]])
     rho = np.moveaxis(rho, source=[0, 1], destination=[i, n+i])
     return rho
 
 
 def apply_superop_2q(rho, super_op, i, j):
     n = rho.ndim // 2
-    rho = np.tensordot(super_op, rho, axes=[[2, 3, 6, 7], [i, j, n+i, n+j]])
+    rho = np.tensordot(super_op, rho, axes=[[4, 5, 6, 7], [i, j, n+i, n+j]])
     rho = np.moveaxis(rho, source=[0, 1, 2, 3], destination=[i, j, n+i, n+j])
     return rho
 
